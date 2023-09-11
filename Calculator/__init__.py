d871fc636d5d3e8471464775635ed6e14ecfc440
CaRn15 <kaapo.karppinen.kaapo@gmail.com>
2023-09-11 18:56:59 +0300

    A simple calculator

---
+++ Calculator/__init__.py
@@ -0,0 +1,27 @@
+def calculate(num1, num2, action):
+    result = None
+    match action:
+        case "+":
+            result = num1 + num2
+        case "-":
+            result = num1 - num2
+        case "*":
+            result = num1 * num2
+        case "/":
+            if num1 == 0:
+                return "Can't devide by 0!"
+            result = num1 / num2
+    return result
+
+if __name__ == "__main__":
+    while True:
+        try:
+            num1 = float(input("Give a first number:"))
+            if isinstance(num1, float):
+                num2 = float(input("Give a second number:"))
+                break
+        except ValueError:
+            print("Invalid input. Please give a number!")
+    action = input("Give a operator (+,-,*,/)")
+    result = calculate(num1, num2, action)
+    print(f"Your result is {result}")
